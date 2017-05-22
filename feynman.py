37e2a3d43b3d3fc38191319e8ea3f596f237baa0
Fionn Fitzmaurice <iamfionn@github.com>
2017-05-22 11:07:10 +0100

    added statistics method

--- feynman.py
+++ feynman.py
@@ -9,28 +9,38 @@ class Particle():
         self.name = particle
         self.charge = self._charge(particle)
         self.lepton = self._lepton(particle)
+        self.statistics = self._statistics(particle)
 
     def _charge(self, particle):
         charge_dict = {'electron': -1, 'positron': 1, 'photon': 0}
         return charge_dict[particle]
 
+
     def _lepton(self, particle):
-        lepton_dict= {'electron': 1, 'positron': -1, 'photon': 0}
+        lepton_dict = {'electron': 1, 'positron': -1, 'photon': 0}
         return lepton_dict[particle]
+
+
+    def _statistics(self, particle):
+        stat_dict = {'electron': 'fermion',
+                     'positron': 'fermion', 
+                     'photon': 'boson'}
+        return stat_dict[particle]
+
 
 PARTICLES = (Particle('electron'), 
              Particle('positron'), 
              Particle('photon'))
 
-def vertex_conservation(particles):
 
+def vertex_conservation(particles):
     lepton_number = sum([p.lepton for p in particles])
     charge = sum([p.charge for p in particles])
 
     return [charge, lepton_number]
 
-def vertex_outputs(constraints, n_out):
 
+def vertex_outputs(constraints, n_out):
     pairs = combine(PARTICLES, n_out)
     allowed_pairs = []
 
@@ -42,14 +52,13 @@ def vertex_outputs(constraints, n_out):
 
 
 if __name__ == "__main__":
-
     particles = ["electron", "positron"]
     particles = [Particle(p) for p in particles]
 
     vertices = 2
     p = particles
+    print([j.name for j in p])
     for i in range(vertices):
-
         constraint = vertex_conservation(p)
         allowed_pairs = vertex_outputs(constraint, 2)
         p = random.choice(allowed_pairs)
